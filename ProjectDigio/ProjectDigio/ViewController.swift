de40957364a2e771dfb20d1a2e8fa0c783545a97
thiagodsmartins3 <thiago.ds.martins@tutanota.com>
2024-05-30 02:11:58 -0300

    first commit

---
+++ ProjectDigio/ProjectDigio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectDigio
+//
+//  Created by Thiago dos Santos Martins on 30/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
